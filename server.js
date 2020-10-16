c839116465bf983ef0ceb7e190adcacf3f2b30a9
jboe26 <joshrboepple@outlook.com>
2020-10-16 12:16:54 -0400

    server and html files

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const http = require('http');
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+})
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -8,6 +14,8 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
+
+app.listen(3000)
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
