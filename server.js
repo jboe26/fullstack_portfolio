9d0f556fa79861f04ba24711834bb2ffaca31174
jboe26 <joshrboepple@outlook.com>
2020-10-17 11:38:43 -0400

    server

--- server.js
+++ server.js
@@ -1,22 +1,54 @@
-const http = require('http');
-const express = require('express');
-const app = express();
+// Dependencies
+var http = require("http");
+var fs = require("fs");
+// var express = require('express');
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
-})
-
+// Set our port to 8080
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+// Create our server
+var server = http.createServer(handleRequest);
 
-app.listen(3000)
+// Create a function for handling the requests and responses coming into our server
+function handleRequest(req, res) {
 
+  // Here we use the fs package to read our index.html file
+  fs.readFile(__dirname + "assets/index.html", function(err, data) {
+    if (err) throw err;
+    // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
+    // an html file.
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+}
+
+// Starts our server
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
+// const http = require('http');
+// const express = require('express');
+// const app = express();
+
+// app.get('/', function (req, res) {
+//     res.send('Hello World');
+// })
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// app.listen(3000)
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
